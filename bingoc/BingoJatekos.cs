553f82bfe9a22033cd031205fedb9c6d85a723f0
szabo.tamas <szabo.tamas@58-03>
2024-06-05 10:10:55 +0200

    Add project files.

---
+++ bingoc/BingoJatekos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bingoc
+{
+    public class BingoJatekos
+    {
+        string[,] tabla;
+        string nev;
+        string[,] talalatok;
+
+        public BingoJatekos(string[,] tabla, string nev)
+        {
+            this.Tabla = tabla;
+            this.Nev = nev;
+        }
+
+        public string[,] Tabla { get => tabla; set => tabla = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public string[,] Talalatok { get => talalatok; set => talalatok = value; }
+    }
+}
---
+++ bingoc/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using bingoc;
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Hello, World!");
+var filename = File.ReadAllLines("nevek.text");
+List<BingoJatekos> lines = new List<BingoJatekos>();
+{
+
+    foreach (var file in filename)
+    {
+        string[] fajl = File.ReadAllLines(file);
+        string[,] tabla = new string[5,5];
+        for (int i = 0; i < 5; i++)
+        {
+            string[] adatok = fajl[i].Split(';');
+            for (int j = 0; j < 5; j++)
+            {
+                tabla[i,j] = adatok[j];
+            }
+        }
+        BingoJatekos bj = new BingoJatekos(tabla,filename.ToString());
+        lines.Add(bj);
+    }
+
+    Console.WriteLine($"darab: {lines.Count()}");
+}
